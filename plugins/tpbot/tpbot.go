4a1c7bd594fb59ca64734d4dd6352fd5d9d2ef96
Donomii <donomii@gmail.com>
2017-07-13 23:31:16 +0200

    Add receive files ability, add send documents ability

--- plugins/tpbot/tpbot.go
+++ plugins/tpbot/tpbot.go
@@ -1,6 +1,10 @@
 package tpbot
 
 import (
+	"strings"
+	"gopkg.in/h2non/filetype.v1"
+	"io/ioutil"
+	"net/http"
 	"log"
 	//"strings"
 	"fmt"
@@ -52,13 +56,25 @@ func (m *Tpbot) OnStart() {
 					bot.SendMessage(message.Chat, "Error creating the new file ", nil)
 
 				} else {
-					picture := telebot.Photo{File: photo}
+					mtype := fileType("botfiles/files/"+m.PayloadString)
+					if strings.HasPrefix(mtype, "image") {
+						picture := telebot.Photo{File: photo}
 
-					err = bot.SendPhoto(message.Chat, &picture, nil)
-					if err != nil {
-						log.Println("Error sending photo")
-						log.Println(err)
-						bot.SendMessage(message.Chat, "Could not send photo", nil)
+						err = bot.SendPhoto(message.Chat, &picture, nil)
+						if err != nil {
+							log.Println("Error sending photo")
+							log.Println(err)
+							bot.SendMessage(message.Chat, "Could not send photo", nil)
+						}
+					} else {
+						d:= telebot.Document{ File: photo, FileName: m.PayloadString, Mime: mtype}
+						err = bot.SendDocument(message.Chat, &d, nil)
+						if err != nil {
+							log.Println("Error sending document")
+							log.Println(err)
+							bot.SendMessage(message.Chat, "Could not send photo", nil)
+						}
+						
 					}
 				}
 			}
@@ -70,11 +86,43 @@ func (m *Tpbot) OnStop() {
 	plugin_registry.UnregisterCommand("p")
 }
 
+func getUrl (url string) []byte {
+resp, err := http.Get(url)
+if err != nil {
+	// handle error
+}
+defer resp.Body.Close()
+body, err := ioutil.ReadAll(resp.Body)
+return body
+}
+
+func fileType (path string) string{
+  buf, _ := ioutil.ReadFile(path)
+
+  kind, unkwown := filetype.Match(buf)
+  if unkwown != nil {
+    return "application/octet-stream"
+  }
+  return kind.MIME.Value
+}
+
 func (m *Tpbot) Run(message telebot.Message) {
 	//config := plugin_registry.Config
 	//if strings.Contains(message.Text, config.CommandPrefix+"p") {
-	fmt.Println(message)
-	bbs.Incoming <- &pbot.BBSmessage{Message: "text", PayloadString: message.Text, UserData: message}
-
+	if message.Chat.Username == "donomii" && message.Chat.Type == "private" {
+	if message.Text == "" {
+		fmt.Printf("%+v\n", message)
+		fileId := message.Document.File.FileID
+		fmt.Printf("FileID: %v\n", fileId)
+		file, _ := plugin_registry.Bot.GetFile(fileId)
+		fmt.Printf("%+v\n",file)
+		url := "https://api.telegram.org/file/bot" + plugin_registry.Config.Token + "/" + file.FilePath
+		data := getUrl(url)
+		err := ioutil.WriteFile("botfiles/files/"+message.Document.FileName, data, 0644) 
+		fmt.Println(err)
+	} else {
+		bbs.Incoming <- &pbot.BBSmessage{Message: "text", PayloadString: message.Text, UserData: message}
+	}
+	}
 	//}
 }
